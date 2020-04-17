9004dabc489651a2ffaaf0d37c27fc4b26573875
KLisabeth <professional.translations4ppl@gmail.com>
2020-04-17 23:57:56 +0200

    deleteExpense listener

--- listeners.js
+++ listeners.js
@@ -19,6 +19,8 @@ function eventListeners() {
     expenseList.addEventListener("click", function () {
         if (event.target.parentElement.classList.contains("edit-icon")) {
             ui.editExpense(event.target.parentElement);
-        } 
+        } else if (event.target.parentElement.classList.contains("delete-icon")) {
+            ui.deleteExpense(event.target.parentElement);
+        }
     });
 }
