d3e482f520931b4dc29d5737318fe990f9e01b4d
KLisabeth <professional.translations4ppl@gmail.com>
2020-04-17 23:31:53 +0200

    showExpenseForm handler

--- handlers.js
+++ handlers.js
@@ -48,4 +48,32 @@ class UI {
           this.balance.classList.add('showBlack')
         }
       }
+      //submit expense form
+    submitExpenseForm(){
+        const expenseValue = this.expenseInput.value;
+        const amountValue =  this.amountInput.value;
+        if(expenseValue === '' || amountValue === '' || amountValue < 0){
+          this.expenseFeedback.classList.add('showItem');
+          this.expenseFeedback.innerHTML = `<p>values cannot be empty or negative</p>`;
+          const self = this;
+          setTimeout(function(){
+            self.expenseFeedback.classList.remove('showItem')
+          }, 4000);
+        }else {
+          let amount = parseInt(amountValue);
+          this.expenseInput.value = '';
+          this.amountInput.value = '';
+  
+          let expense = {
+            id:this.itemID,
+            title:expenseValue,
+            amount:amount,
+          };
+          this.itemID++;
+          this.itemList.push(expense);
+          this.addExpense(expense);
+          this.showBalance();
+        }
+      }
+  
 }
