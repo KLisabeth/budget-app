b02efbf2d508774c6177b5f1230169a2974fd638
KLisabeth <professional.translations4ppl@gmail.com>
2020-04-17 23:55:23 +0200

    editExpense handler

--- handlers.js
+++ handlers.js
@@ -105,4 +105,26 @@ class UI {
         this.expenseAmount.textContent = total;
         return total;
       }
+       // edit expense
+     editExpense(element) {
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+        // remove from dom
+        this.expenseList.removeChild(parent);
+        //remove from the list;
+        let expense = this.itemList.filter(function (item) {
+            return item.id === id;
+        });
+        // show value
+        this.expenseInput.value = expense[0].title;
+        this.amountInput.value = expense[0].amount;
+        // remove from list
+        let tempList = this.itemList.filter(function (expense) {
+            return expense.id !== id;
+        });
+  
+        this.itemList = tempList;
+        this.showBalance();
+    }
+    
 }
